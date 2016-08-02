7fac480bf008bcf2dd46e23a1013b31bbced9fb7
Marc Guyer <marcguyer@gmail.com>
2016-08-02 16:04:25 -0400

    OpenSSL impl for PHP

--- php.php
+++ php.php
@@ -1,19 +1,19 @@
-&lt;?php
+<?php
 $account_key = "YOUR_ACCOUNT_KEY";
 $api_key = "YOUR_API_KEY";
 
 $salted = $api_key . $account_key;
 $hash = hash('sha1',$salted,true);
 $saltedHash = substr($hash,0,16);
-$iv = "OpenSSL for Ruby";
+$iv = "OpenSSL for -PHP";
 
 $user_data = array(
-  "guid" => "<%= example_user[:guid] %>",
-  "expires" => "<%= example_user[:expires].to_s(:db) %>",
-  "display_name" => "<%= example_user[:display_name] %>",
-  "email" => "<%= example_user[:email] %>",
-  "url" => "<%= example_user[:url] %>",
-  "avatar_url" => "<%= example_user[:avatar_url] %>"
+  "guid"          => "example_user[:guid]",
+  "display_name"  => "example_user[:display_name]",
+  "email"         => "example_user[:email]",
+  "url"           => "example_user[:url]",
+  "avatar_url"    => "example_user[:avatar_url]",
+  "expires"       => date("r", strtotime("+30 minutes"))
 );
 
 $data = json_encode($user_data);
@@ -26,11 +26,14 @@ for ($i = 0; $i < 16; $i++)
 
 $pad = 16 - (strlen($data) % 16);
 $data = $data . str_repeat(chr($pad), $pad);
-	
-$cipher = mcrypt_module_open(MCRYPT_RIJNDAEL_128,'','cbc','');
-mcrypt_generic_init($cipher, $saltedHash, $iv);
-$encryptedData = mcrypt_generic($cipher,$data);
-mcrypt_generic_deinit($cipher);
+
+$encryptedData = openssl_encrypt(
+	$data,
+	'aes-128-cbc',
+	$saltedHash,
+	OPENSSL_RAW_DATA|OPENSSL_ZERO_PADDING,
+	$iv
+);
 
 $encryptedData = urlencode(base64_encode($encryptedData));
-?&gt;
+?>
